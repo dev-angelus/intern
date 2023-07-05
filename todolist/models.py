2fdcb29a78db772a66aa987b181589aaf2687b3e
dev-angelus <abdillaeva.n@gmail.com>
2023-07-05 16:47:25 +0600

    todolist1

--- todolist/models.py
+++ todolist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Task(models.Model):
-    id = models.AutoField(primary_key=True)
+    id_task = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
